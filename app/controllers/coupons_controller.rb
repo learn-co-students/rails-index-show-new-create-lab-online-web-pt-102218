1d569fc84efc27a7055c2aa1ea8438979f011d8a
grant smith <grantsmith1256@gmail.com>
2019-03-25 18:28:54 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,4 +4,16 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
   end
 
+  def show
+    @coupon = Coupon.find_by(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+    redirect_to coupon_path(@coupon)
+  end
+
 end
