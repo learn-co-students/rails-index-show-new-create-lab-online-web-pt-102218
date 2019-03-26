abb2f7ada2ba3799fa829aa3e3c850d325e4413c
jaywood128 <jaywood128@gmail.com>
2019-03-26 13:26:07 -0700

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class CouponsController < ApplicationController
+
+    def index 
+        @coupons = Coupon.all 
+    end
+
+    def new 
+        @coupon = Coupon.new
+    end
+
+    def create 
+        @coupon = Coupon.new 
+        @coupon.coupon_code = params[:coupon_code]
+        @coupon.store = params[:store]
+        @coupon.save
+        redirect_to coupon_path(@coupon)
+    end
+
+    def show 
+    end
+
+end
